69996fd284be8fae2c8f7b539d73159d27d9825f
Héctor Ramón Jiménez <hector0193@gmail.com>
2019-07-01 17:24:26 +0200

    Implement surface-to-surface copy in GL backend

--- src/backend/gl/src/queue.rs
+++ src/backend/gl/src/queue.rs
@@ -698,8 +698,44 @@ impl CommandQueue {
             com::Command::CopyImageToTexture(..) => {
                 unimplemented!() //TODO: use FBO
             }
-            com::Command::CopyImageToSurface(..) => {
-                unimplemented!() //TODO: use FBO
+            com::Command::CopyImageToSurface(image_kind, dst, ref data) => {
+                let gl = &self.share.context;
+
+                match image_kind {
+                    native::ImageKind::Texture { .. } => {
+                        unimplemented!()
+                    }
+                    native::ImageKind::Surface(src) => {
+                        unsafe {
+                            let src_fbo = gl.create_framebuffer().unwrap();
+                            gl.bind_framebuffer(glow::READ_FRAMEBUFFER, Some(src_fbo));
+                            gl.framebuffer_renderbuffer(glow::READ_FRAMEBUFFER, glow::COLOR_ATTACHMENT0, glow::RENDERBUFFER, Some(src));
+
+                            let dst_fbo = gl.create_framebuffer().unwrap();
+                            gl.bind_framebuffer(glow::DRAW_FRAMEBUFFER, Some(dst_fbo));
+                            gl.framebuffer_renderbuffer(glow::DRAW_FRAMEBUFFER, glow::COLOR_ATTACHMENT0, glow::RENDERBUFFER, Some(dst));
+
+                            gl.blit_framebuffer(
+                                data.src_offset.x,
+                                data.src_offset.y,
+                                data.src_offset.x + data.extent.width as i32,
+                                data.src_offset.y + data.extent.height as i32,
+                                data.dst_offset.x,
+                                data.dst_offset.y,
+                                data.dst_offset.x + data.extent.width as i32,
+                                data.dst_offset.y + data.extent.height as i32,
+                                glow::COLOR_BUFFER_BIT,
+                                glow::LINEAR,
+                            );
+
+                            gl.bind_framebuffer(glow::READ_FRAMEBUFFER, None);
+                            gl.bind_framebuffer(glow::DRAW_FRAMEBUFFER, None);
+
+                            gl.delete_framebuffer(src_fbo);
+                            gl.delete_framebuffer(dst_fbo);
+                        }
+                    }
+                }
             }
             com::Command::BindBufferRange(target, index, buffer, offset, size) => unsafe {
                 let gl = &self.share.context;
